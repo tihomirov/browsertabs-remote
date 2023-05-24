6a522e9086246e3a01e3dab95fcb0ca7bf696048
Anton <antontikhomirov93@gmail.com>
2023-05-24 20:00:49 +0300

    Add test socket connection

--- App.tsx
+++ App.tsx
@@ -1,10 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [messages, setMessages] = useState<string[]>([]);
+
+  useEffect(() => {
+    const socket = new WebSocket('ws://192.168.31.111:8999');
+
+    // Connection opened
+    socket.addEventListener('open', event => {
+      socket.send('Hello Server!');
+    });
+
+    // Listen for messages
+    socket.addEventListener('message', event => {
+      const message: string = event.data;
+      console.log('Message from server', message);
+      setMessages([
+        ...messages,
+        message
+      ])
+    });
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Open up App.tsx to start working on your app!!</Text>
+      {messages.map((message, index) => (<Text key={index}>{index} - {message}</Text>))}
       <StatusBar style="auto" />
     </View>
   );
