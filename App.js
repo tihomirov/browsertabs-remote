2b7a29c224799d15e6c511087a23a8d6f79c8f23
Anton <antontikhomirov93@gmail.com>
2023-08-17 12:38:07 +0300

    TMP Inited webRTC

--- App.js
+++ App.js
@@ -1,6 +1,47 @@
+import { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {
+	ScreenCapturePickerView,
+	RTCPeerConnection,
+	RTCIceCandidate,
+	RTCSessionDescription,
+	RTCView,
+	MediaStream,
+	MediaStreamTrack,
+	mediaDevices,
+	registerGlobals
+} from 'react-native-webrtc';
+
+registerGlobals();
+let inited = false
 
 export default function App() {
+
+  useEffect(() => {
+    if (inited) {
+      return;
+    }
+    let peerConstraints = {
+      iceServers: [
+        {
+          urls: 'stun:stun.l.google.com:19302'
+        }
+      ]
+    };
+    let peerConnection = new RTCPeerConnection( peerConstraints );
+
+    peerConnection.addEventListener( 'connectionstatechange', event => {} );
+    peerConnection.addEventListener( 'icecandidate', event => {} );
+    peerConnection.addEventListener( 'icecandidateerror', event => {} );
+    peerConnection.addEventListener( 'iceconnectionstatechange', event => {} );
+    peerConnection.addEventListener( 'icegatheringstatechange', event => {} );
+    peerConnection.addEventListener( 'negotiationneeded', event => {} );
+    peerConnection.addEventListener( 'signalingstatechange', event => {} );
+    peerConnection.addEventListener( 'track', event => {} );
+
+    inited = true;
+  }, [])
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
