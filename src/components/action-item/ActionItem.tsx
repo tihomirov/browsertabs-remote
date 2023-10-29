be0ac1c11d2ec41c218a6132d1fb7c88ac5175a8
Anton <antontikhomirov93@gmail.com>
2023-10-29 17:31:28 +0200

    Update Action List

--- src/components/action-item/ActionItem.tsx
+++ src/components/action-item/ActionItem.tsx
@@ -1,29 +1,75 @@
 import {ActionType} from 'browsertabs-remote-common/src/common';
-import {FC} from 'react';
-import {ListRenderItem} from 'react-native';
+import {FC, memo} from 'react';
+import {Divider,List} from 'react-native-paper';
 
-import {ActionItemClose} from './ActionItemClose';
-import {ActionItemCreate} from './ActionItemCreate';
-import {ActionItemDecreaseZoom} from './ActionItemDecreaseZoom';
-import {ActionItemIncreaseZoom} from './ActionItemIncreaseZoom';
-import {ActionItemReload} from './ActionItemReload';
-import {ActionItemSetZoom} from './ActionItemSetZoom';
-import {ActionItemToggleMute} from './ActionItemToggleMute';
+// import {ActionItemClose} from './ActionItemClose';
+// import {ActionItemCreate} from './ActionItemCreate';
+// import {ActionItemDecreaseZoom} from './ActionItemDecreaseZoom';
+// import {ActionItemIncreaseZoom} from './ActionItemIncreaseZoom';
+// import {ActionItemReload} from './ActionItemReload';
+// import {ActionItemSetZoom} from './ActionItemSetZoom';
+// import {ActionItemToggleMute} from './ActionItemToggleMute';
 
-const actionItemComponent: Record<ActionType, FC> = {
-  [ActionType.Close]: ActionItemClose,
-  [ActionType.Create]: ActionItemCreate,
-  [ActionType.DecreaseZoom]: ActionItemDecreaseZoom,
-  [ActionType.IncreaseZoom]: ActionItemIncreaseZoom,
-  [ActionType.Reload]: ActionItemReload,
-  [ActionType.SetZoom]: ActionItemSetZoom,
-  [ActionType.ToggleMute]: ActionItemToggleMute,
+// const actionItemComponent: Record<ActionType, FC> = {
+//   [ActionType.Close]: ActionItemClose,
+//   [ActionType.Create]: ActionItemCreate,
+//   [ActionType.DecreaseZoom]: ActionItemDecreaseZoom,
+//   [ActionType.IncreaseZoom]: ActionItemIncreaseZoom,
+//   [ActionType.Reload]: ActionItemReload,
+//   [ActionType.SetZoom]: ActionItemSetZoom,
+//   [ActionType.ToggleMute]: ActionItemToggleMute,
+// };
+
+type ActionItemProps = Readonly<{
+  actionType: ActionType;
+}>;
+
+type ActionData = Readonly<{
+  icon: string;
+  title: string;
+}>;
+
+const actionsData: Record<ActionType, ActionData> = {
+  [ActionType.Reload]: {
+    icon: 'reload',
+    title: 'Reload',
+  },
+  [ActionType.Close]: {
+    icon: 'close',
+    title: 'Close',
+  },
+  [ActionType.ToggleMute]: {
+    icon: 'volume-source',
+    title: 'Toggle Mute',
+  },
+  [ActionType.IncreaseZoom]: {
+    icon: 'magnify-plus-outline',
+    title: 'Increase Zoom',
+  },
+  [ActionType.DecreaseZoom]: {
+    icon: 'magnify-minus-outline',
+    title: 'Decrease Zoom',
+  },
+  [ActionType.SetZoom]: {
+    icon: 'magnify',
+    title: 'Set Zoom',
+  },
+  [ActionType.Create]: {
+    icon: 'plus',
+    title: 'Create',
+  },
 };
 
-export const listRenderActionItem: ListRenderItem<ActionType> = (
-  {item}
-) => {
-  const Component = actionItemComponent[item];
-
-  return <Component />;
-};
+export const ActionItem: FC<ActionItemProps> = memo(function ActionItem({actionType}) {
+  const actionData = actionsData[actionType];
+  return (
+    <>
+      <List.Item
+        title={actionData.title}
+        left={() => <List.Icon icon={actionData.icon} />}
+        right={() => <List.Icon icon={actionData.icon} />}
+      />
+      <Divider />
+    </>
+  );
+});
--- src/components/action-list/ActionList.tsx
+++ src/components/action-list/ActionList.tsx
@@ -1,9 +1,10 @@
 import {ActionType} from 'browsertabs-remote-common/src/common';
 import {observer} from 'mobx-react-lite';
 import {FC} from 'react';
-import {FlatList, StyleSheet,View} from 'react-native';
+import {StyleSheet} from 'react-native';
+import {List} from 'react-native-paper';
 
-import {listRenderActionItem} from '../../components/action-item';
+import {ActionItem} from '../../components/action-item';
 
 export type ActionListProps = Readonly<{
   actions: ReadonlyArray<ActionType>;
@@ -12,30 +13,15 @@ export type ActionListProps = Readonly<{
 export const ActionList: FC<ActionListProps> = observer(({actions}) => {
 
   return (
-    <View style={styles.container}>
-      <View style={styles.listContainer}>
-        <FlatList
-          scrollEnabled={false}
-          data={actions}
-          renderItem={listRenderActionItem}
-          keyExtractor={item => item.toString()}
-        />
-      </View>
-    </View>
+    <List.Section>
+      <List.Subheader style={styles.subheader}>Actions</List.Subheader>
+      {actions.map((action, index) => (<ActionItem key={index} actionType={action} />))}
+    </List.Section>
   );
 });
 
 const styles = StyleSheet.create({
-  container: {
-    height: '100%',
-    display: 'flex',
-    paddingVertical: 12,
-    justifyContent: 'space-between',
-  },
-  listContainer: {
-    flexGrow: 1,
-  },
-  buttonContainer: {
-    paddingHorizontal: '10%',
+  subheader: {
+    alignSelf: 'center'
   }
 });
